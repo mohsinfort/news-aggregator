fa3b1d602ad7d9e6a23821f524273c2494b4da56
mohsinfort <mohsinfort@gmail.com>
2024-10-08 10:55:53 +0500

    create general lang file

---
+++ lang/en/general.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | General Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the general error messages used by
+    | different modules of application. Feel free to tweak each of these messages
+    | here.
+    |
+    */
+
+    'notFound' => 'Not found',
+    'invalidData' => 'The given data was invalid.',
+    'pleaseContactSupportWithCode' => 'Unexpected error occurred, please contact support. Error code: :code',
+    'wentWrong' => 'Something went wrong',
+];
