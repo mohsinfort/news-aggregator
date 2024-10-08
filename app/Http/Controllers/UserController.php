058e6e5673d8c8021a3f36c3126998707e5ead0d
mohsinfort <mohsinfort@gmail.com>
2024-10-08 10:25:59 +0500

    validate register user request by form request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Auth\RegisterUserRequest;
+
 class UserController extends Controller
 {
-    public function register()
+    public function register(RegisterUserRequest $request)
     {
-        
+        dd($request->all());
     }
 }
