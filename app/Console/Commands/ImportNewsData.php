0b6a24ad493b74f7b98c569fd6ff139dde02f535
mohsinfort <mohsinfort@gmail.com>
2024-10-09 19:14:17 +0500

    add console command to dispatch import job

---
+++ app/Console/Commands/ImportNewsData.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\ImportNewsDataJob;
+use Illuminate\Console\Command;
+
+class ImportNewsData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:news-data';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import news data from different channels';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        ImportNewsDataJob::dispatch();//->onQueue('low');
+
+        $this->info('Import news data from different channels');
+    }
+}
