a5714ed17f5b81b95cb320e04bbb18747508c75a
mohsinfort <mohsinfort@gmail.com>
2024-10-10 11:01:27 +0500

    add relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function userPrefrence()
+    {
+        return $this->hasOne(UserPreference::class);
+    }
 }
