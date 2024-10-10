a5577e75519e90f777e5b055cefa53fb21a067f8
mohsinfort <mohsinfort@gmail.com>
2024-10-10 10:35:30 +0500

    add fillable property

--- app/Models/UserPreference.php
+++ app/Models/UserPreference.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class UserPreference extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'news_type',
+        'news_source',
+        'news_author',
+    ];
 }
